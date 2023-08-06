ca3f04c059de4bd5dbd2be8e87527b72321bd2bd
Eddie <eddie@edwinfairchild.com>
2023-08-06 17:35:22 -0500

    add tfluna python driver

--- BLE_FreeRTOS/build/startup_max32655.d
+++
@@ -1,2 +0,0 @@
-/home/eddie/projects/cubeAlert/BLE_FreeRTOS/build/startup_max32655.o: \
- /home/eddie/workspace/msdk/Libraries/CMSIS/Device/Maxim/MAX32655/Source/GCC/startup_max32655.S
--- BLE_FreeRTOS/build/wsf_detoken.d
+++
@@ -1,2 +0,0 @@
-/home/eddie/projects/cubeAlert/BLE_FreeRTOS/build/wsf_detoken.o: \
- /home/eddie/workspace/msdk/Libraries/Cordio/wsf/sources/targets/freertos/wsf_detoken.c
